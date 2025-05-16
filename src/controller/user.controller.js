f0b84dd759970203022f70a8e56a8e1f1925f028
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-05-16 15:22:49 +0530

    commit userRegister

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -7,7 +7,26 @@ import { userModel } from "../model/user.model.js"
 
 export const userRegister = async (req,res) => {
 
+    try {
 
+        const {name,password,email} = req.body;
+
+        if(!name || !password || !email){
+
+          return  res.status(404).json({error: "All params Not Found"});
+
+        }
+
+        const user = await userModel.create({name,password,email});
+
+        return res.status(200).json({message: "User Register Successfully",user});
+        
+    } catch (error) {
+       
+        console.error(error);
+        return  res.status(500).json({error: "Internal Server Error"});
+
+    }
 
 }
 
